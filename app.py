0a82ccfceba01892f2c7f0b323c4c87816fe3039
Thomas von Stetten <52258597+tvstetten@users.noreply.github.com>
2023-02-22 13:27:29 +0000

    Added jobpage, dynamic job-pages. shared layout

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -10,8 +10,15 @@ def hello_world():
                          company_name="Jovian Careers Inc.")
 
 
+@app.route("/job/<id>")
+def job_json(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template("jobpage.html", job=job)
+
 @app.route("/api/jobs")
-def jobs_json():
+def api_jobs_json():
   return jsonify(load_jobs_from_db())
 
 
--- database.py
+++ database.py
@@ -16,6 +16,16 @@ def load_jobs_from_db():
       result_jobs.append(row._asdict())
   return result_jobs
 
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"), {"val": id})
+
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return rows[0]._asdict()
+
 
 # with engine.connect() as conn:
 #   result = conn.execute(text("select * from jobs"))
